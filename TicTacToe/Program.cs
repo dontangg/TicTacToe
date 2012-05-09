43e498956a1ec465275331ff312389a1a269ce51
Don Wilson <robert.don.wilson@gmail.com>
2012-05-09 08:22:24 -0600

    Do a little cleanup

--- TicTacToe/Program.cs
+++ TicTacToe/Program.cs
@@ -9,7 +9,7 @@ namespace TicTacToe
 	{
 		static void Main(string[] args)
 		{
-			bool stillPlaying = true;
+			var stillPlaying = true;
 
 			Console.ForegroundColor = ConsoleColor.DarkGreen;
 			Console.WriteLine("-----------------------");
@@ -25,7 +25,7 @@ namespace TicTacToe
 
 				Console.Write("Type a number and hit <enter>: ");
 
-				string choice = GetUserInput("[12]");
+				var choice = GetUserInput("[12]");
 
 				switch(choice)
 				{
@@ -63,25 +63,24 @@ namespace TicTacToe
 				Console.Write("How many rows do you want to have? (3, 4, or 5) ");
 				numRowsChoice = GetUserInput("[345]");
 			}
-
-			var board = new string[9];
+			var boardSize = (int)Math.Pow(int.Parse(numRowsChoice), 2);
+			var board = new string[boardSize];
 
 			DrawBoard(board);
 
 			// TODO:
-			// 1. create an string array that will store the positions of all 9 spaces on the board
-			// 2. create a method that will take the string array as a parameter and return a string
+			// * create a method that will take the string array as a parameter and return a string
 			//    indicating who wins (null = no win, "X" = x wins, "O" = o wins). Use IsBoardFull() as an example
-			// 3. make a loop that keeps going until either someone has won or the board is full
-			// 4. create a method that takes the string array as a parameter and can output the board
-			// 5. ask the user to type a number indicating where they would like to put their x or o
-			// 6. make sure that the user typed in a valid option
-			// 7. announce the result of the game
+			// * make a loop that keeps going until either someone has won or the board is full
+			// * create a method that takes the string array as a parameter and can output the board
+			// * ask the user to type a number indicating where they would like to put their x or o
+			// * make sure that the user typed in a valid option
+			// * announce the result of the game
 		}
 
 		private static void DrawBoard(string[] board)
 		{
-			int numRows = (int)Math.Sqrt(board.Length);
+			var numRows = (int)Math.Sqrt(board.Length);
 
 			for (int row = 0; row < numRows; row++)
 			{
@@ -105,28 +104,6 @@ namespace TicTacToe
 		private static bool IsBoardFull(string[] board)
 		{
 			return board.All(space => space != null);
-		}
-
-		private static bool IsBoardFullAlt(string[] board)
-		{
-			for (int i = 0; i < board.Length; i++)
-			{
-				if (board[i] == null)
-					return false;
-			}
-
-			return true;
-		}
-
-		private static bool IsBoardFullAlt2(string[] board)
-		{
-			foreach (string space in board)
-			{
-				if (space == null)
-					return false;
-			}
-
-			return true;
 		}
 	}
 }
